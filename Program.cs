28c16fb95451e40225c1ed9f7a7aa6622b878fe3
johngaringo51 <jonald.manipon@yahoo.co.nz>
2021-06-08 19:49:08 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Greater_of_two_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("Please enter first number");
+            number1 = Convert.ToInt32(Console.ReadLine()); //This statement is converting into int
+
+            Console.WriteLine("Please enter second number");
+            number2 = Convert.ToInt32(Console.ReadLine()); //This statement is converting into int
+
+            if (number1 > number2)
+            {
+                Console.WriteLine(number1.ToString() + " is greater than " + number2.ToString());
+            }
+            else
+            {
+
+            }
+            Console.WriteLine("Number 2 is greater number");
+        }
+    }
+
+}
